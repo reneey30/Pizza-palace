85057a1433fc060a5e37df7c3f52aa143cafeb22
Irene Njeru <irenenjeru8@gmail.com>
2022-03-19 22:06:02 +0300

    add a final message that gave your order and delivery details

--- js/script.js
+++ js/script.js
@@ -161,31 +161,31 @@ $(document).ready(function(){
       $("#totalbill").append("Your bill plus delivery fee is: "+deliveryamount);
     });
 
-          // when one clicks place order button
+        // when one clicks place order button
     $("button#final-order").click(function(event){
-        event.preventDefault();
+      event.preventDefault();
+
+      $("#pizzatotal").hide();
+      $(".delivery").hide();
+      $("button#final-order").hide();
+      let deliveryamount= checkoutTotal+200;
+      console.log("Final Bill is: "+deliveryamount);
+      let person = $("input#name").val();
+      let phone = $("input#phone").val();
+      let location = $("input#location").val();
+
+      if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
   
-        $("#pizzatotal").hide();
-        $(".delivery").hide();
-        $("button#final-order").hide();
-        let deliveryamount= checkoutTotal+150;
-        console.log("Final Bill is: "+deliveryamount);
-        let person = $("input#name").val();
-        let phone = $("input#phone").val();
-        let location = $("input#location").val();
-  
-        if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
-    
-          $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliceryamount);
-          $("#totalbill").hide();
-          $("#finallmessage").slideDown(1200);
-        }
-        else {
-          alert("Please fill in the details for delivery!");
-          $(".delivery").show();
-          $("button#final-order").show();
-        }
-      });
-     event.preventDefault();
+        $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliveryamount);
+        $("#totalbill").hide();
+        $("#finallmessage").slideDown(1200);
+      }
+      else {
+        alert("Please fill in the details for delivery!");
+        $(".delivery").show();
+        $("button#final-order").show();
+      }
     });
-  });
+   event.preventDefault();
+});
+});
