22bbbc67de2d745ae6f1effc0b12c582f1d75593
Irene Njeru <irenenjeru8@gmail.com>
2022-03-19 21:57:02 +0300

    add more on the proceed button and paraphased some changes

--- js/script.js
+++ js/script.js
@@ -9,76 +9,78 @@ function getpizza( name,size,crust,topping, total ){
 }
 // proceed button
 $(document).ready(function(){
-    $("button.proceed").click(function(event){
-        let pname = $(".name option:selected").val();
-        let psize = $("#size option:selected").val();
-        let pcrust = $("#crust option:selected").val();
-        let ptopping = [];
-        $.each($("input[name='toppings']:checked"), function(){            
-            ptopping.push($(this).val());
-        });
-        console.log(ptopping.join(", "));
-     
-        switch(psize){
+  $("button#proceed").click(function(event){
+      let pname = $("#name option:selected").val();
+      let psize = $("#size option:selected").val();
+      let pcrust = $("#crust option:selected").val();
+      let ptopping = [];
+      $.each($("input[name='toppings']:checked"), function(){            
+          ptopping.push($(this).val());
+      });
+      console.log(ptopping.join(", "));
+   
+      switch(psize){
+       case "0":
+         price =0;
+       break;
+       case "large":
+          price = 1000;
+          console.log(price);
+        break;
+        case "medium":
+          price = 750;
+          console.log("The price is "+price);
+        break;
+        case "small":
+          price = 600;
+          console.log(price);
+        default:
+          console.log("error"); 
+      }
+      switch(pcrust){
          case "0":
-           price =0;
+           crust_price = 0;
          break;
-         case "large":
-            price = 1000;
-            console.log(price);
-          break;
-          case "medium":
-            price = 750;
-            console.log("The price is "+price);
-          break;
-          case "small":
-            price = 600;
-            console.log(price);
-          default:
-            console.log("error"); 
-        }
-        switch(pcrust){
-           case "0":
-             crust_price = 0;
-           break;
-           case "Thick Crust":
-             crust_price = 250;
-           break;
-           case "Stuffed":
-             crust_price = 200;
-           break;
-           case "Cracker Crust":
-             crust_price = 150;
-           break;
-           default:
-             console.log("No price"); 
-         }
-         let topping_value = ptopping.length*50;
-         console.log("toppins value" + topping_value);
-     
-         if((psize == "0") && (pcrust == "0")){
-           console.log("nothing selected");
-           $("button.proceed").show();
-           $("#information").show();
-           $("div.choice").hide();
-           alert("Please select pizza size and crust"); 
-         }
-         else{
-           $("button.proceed").hide();
-           $("#information").hide();
-           $("div.choice").slideDown(1000);
-         }
-     
-         total = price + crust_price + topping_value;
-         console.log(total);
-         let checkoutTotal =0;
-         checkoutTotal = checkoutTotal + total;
-     
-         $("#pizzaname").html($(".name option:selected").val());
-         $("#pizzasize").html( $("#size option:selected").val());
-         $("#pizzacrust").html($("#crust option:selected").val());
-         $("#pizzatopping").html(ptopping.join(", "));
-         $("#totals").html(total);
+         case "Thick-Crust":
+           crust_price = 250;
+         break;
+         case "Stuffed":
+           crust_price = 200;
+         break;
+         case "Cracker-Crust":
+           crust_price = 150;
+         break;
+         default:
+           crust_price = 0;
+           console.log("No price"); 
+       }
+       let topping_value = ptopping.length*50;
+       console.log("toppins value" + topping_value);
+   
+       if((psize == "0") && (pcrust == "0")){
+         console.log("nothing selected");
+         $("button#proceed").show();
+         $("#information").show();
+         $("div.choice").hide();
+         alert("Please select pizza size and crust"); 
+       }
+       else{
+         $("button#proceed").hide();
+         $("#information").hide();
+         $("div.choice").slideDown(1000);
+       }
+   
+       total = price + crust_price + topping_value;
+       console.log(total);
+       let checkoutTotal =0;
+       checkoutTotal = checkoutTotal + total;
+   
+       $("#pizzaname").html($("#name option:selected").val());
+       $("#pizzasize").html( $("#size option:selected").val());
+       $("#pizzacrust").html($("#crust option:selected").val());
+       $("#pizzatopping").html(ptopping.join(", "));
+       $("#totals").html(total);
+  //});
 
     //      // Add pizza button
     // $("button.addPizza").click(function(){
