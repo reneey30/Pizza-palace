3fb46f1332ae6299e6c3bb146bb2a7da1f3725bd
Irene Njeru <irenenjeru8@gmail.com>
2022-03-19 22:04:22 +0300

    made corrections on words

--- js/script.js
+++ js/script.js
@@ -139,28 +139,27 @@ $(document).ready(function(){
      $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
      console.log(newOrder);
 });
-         // Checkout button
+          // Checkout button
     $("button#checkout").click(function(){ 
-        $("button#checkout").hide();
-        $("button.addPizza").hide();
-        $("button.deliver").slideDown(1000);
-        $("#addedprice").slideDown(1000);
-        console.log("Your total bills is sh. "+checkoutTotal);
-        $("#pizzatotal").append("Your bill is sh. "+checkoutTotal);
-      });
-        // home delivery button
-    $("button.deliver").click(function(){
-        $(".pizzatable").hide();
-        $(".choise h2").hide();
-        $(".delivery").slideDown(1000);
-        $("#addedprice").hide();
-        $("button.deliver").hide();
-        $("#pizzatotal").hide();
-        let deliceryamount= checkoutTotal+200;
-        console.log("You will pay sh. "+deliceryamount+" on delivery");
-        $("#totalbill").append("Your bill plus delivery fee is: "+deliceryamount);
-      });
-  
+      $("button#checkout").hide();
+      $("button#addPizza").hide();
+      $("button#deliver").slideDown(1000);
+      $("#addedprice").slideDown(1000);
+      console.log("Your total bills is sh. "+checkoutTotal);
+      $("#pizzatotal").append("Your bill is sh. "+checkoutTotal);
+    });
+      // home delivery button
+  $("button#deliver").click(function(){
+      $(".pizzatable").hide();
+      $(".choise h2").hide();
+      $(".delivery").slideDown(1000);
+      $("#addedprice").hide();
+      $("button#deliver").hide();
+      $("#pizzatotal").hide();
+      let deliveryamount= checkoutTotal+200;
+      console.log("You will pay sh. "+deliveryamount+" on delivery");
+      $("#totalbill").append("Your bill plus delivery fee is: "+deliveryamount);
+    });
 
           // when one clicks place order button
     $("button#final-order").click(function(event){
