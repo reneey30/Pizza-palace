f431998e33384188fb18d9b921b4f349b1843911
Irene Njeru <irenenjeru8@gmail.com>
2022-03-19 21:59:11 +0300

    add the add pizza button that is funtionable

--- js/script.js
+++ js/script.js
@@ -81,63 +81,55 @@ $(document).ready(function(){
        $("#pizzatopping").html(ptopping.join(", "));
        $("#totals").html(total);
   //});
-
-    //      // Add pizza button
-    // $("button.addPizza").click(function(){
-    //     let pname = $(".name option:selected").val();
-    //     let psize = $("#size option:selected").val();
-    //     let pcrust = $("#crust option:selected").val();
-    //     let ptopping = [];
-    //     $.each($("input[name='toppings']:checked"), function(){            
-    //         ptopping.push($(this).val());
-    //     });
-    //     console.log(ptopping.join(", "));
-    //     switch(psize){
-    //       case "0":
-    //         price =0;
-    //       break;
-    //       case "large":
-    //          price = 1000;
-    //          console.log(price);
-    //        break;
-    //        case "medium":
-    //          price = 750;
-    //          console.log("The price is "+price);
-    //        break;
-    //        case "small":
-    //          price = 600;
-    //          console.log(price);
-    //        default:
-    //          console.log("error"); 
-    //      }
-    //      switch(pcrust){
-    //         case "0":
-    //           crust_price = 0;
-    //         break;
-    //         case "Thick Crust":
-    //           crust_price = 250;
-    //         break;
-    //         case "Stuffed":
-    //           crust_price = 200;
-    //         break;
-    //         case "Cracker crust":
-    //           crust_price = 150;
-    //         break;
-    //         default:
-    //           console.log("No price"); 
-    //       }
-    //       let topping_value = ptopping.length*50;
-    //       console.log("toppins value" + topping_value);
-    //       total = price + crust_price + topping_value;
-    //       console.log(total);
-  
-    //       checkoutTotal = checkoutTotal + total;
-    //       console.log(checkoutTotal);
-        // // constractor function
-        // var newOrder = new Getpizza(pname, psize, pcrust,ptopping,total);
-  
-        // $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
-        // console.log(newOrder);
+  // Add pizza button
+  $("button#addPizza").click(function(){
+    let pname = $("#name option:selected").val();
+    let psize = $("#size option:selected").val();
+    let pcrust = $("#crust option:selected").val();
+    let ptopping = [];
+    $.each($("input[name='toppings']:checked"), function(){            
+        ptopping.push($(this).val());
+    });
+    console.log(ptopping.join(", "));
+    switch(psize){
+      case "0":
+        price =0;
+      break;
+      case "large":
+         price = 1000;
+         console.log(price);
+       break;
+       case "medium":
+         price = 750;
+         console.log("The price is "+price);
+       break;
+       case "small":
+         price = 600;
+         console.log(price);
+       default:
+         console.log("error"); 
+     }
+     switch(pcrust){
+        case "0":
+          crust_price = 0;
+        break;
+        case "Thick-Crust":
+          crust_price = 250;
+        break;
+        case "Stuffed":
+          crust_price = 200;
+        break;
+        case "Cracker-crust":
+          crust_price = 150;
+        break;
+        default:
+          crust_price = 0;
+          console.log("No price"); 
+      }
+      let topping_value = ptopping.length*50;
+      console.log("toppins value" + topping_value);
+      total = price + crust_price + topping_value;
+      console.log(total);
          // Checkout button
     $("button#checkout").click(function(){ 
         $("button#checkout").hide();
